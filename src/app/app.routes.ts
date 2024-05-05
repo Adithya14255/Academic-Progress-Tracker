4b74e93d7bfc484c33b67ecef3ef2f4f49bb4a95
Dream <138191954+rajiv2004-cloud@users.noreply.github.com>
2024-05-05 12:24:17 +0530

    Completed path and set default page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { CourseMentorPortralComponent } from './course-mentor-portral/course-mentor-portral.component';
+import { CourseCoordinatorPortralComponent } from './course-coordinator-portral/course-coordinator-portral.component';
+import { DomainMentorPortraltComponent } from './domain-mentor-portralt/domain-mentor-portralt.component';
+import { HodDashboardComponent } from './hod-dashboard/hod-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LoginComponent },
+    { path: 'course-mentor', component: CourseMentorPortralComponent},
+    { path: 'course-coordinator', component: CourseCoordinatorPortralComponent},
+    { path: 'domain-mentor', component: DomainMentorPortraltComponent},
+    { path: 'hod-dashboard', component: HodDashboardComponent},
+];
