8633a450d587baeca347c4b482fc6b55f7c108cc
unknown <adithya14255@gmail.com>
2024-06-05 00:42:01 +0530

    last

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 app.secret_key="helloworld"
-engine = sqlalchemy.create_engine("postgresql://admin:admin@172.16.45.117/kgaps")
+engine = sqlalchemy.create_engine("postgresql://admin:admin@192.168.0.247/kgaps")
 conn = engine.connect()
 
 
@@ -18,12 +18,12 @@ def index():
     return json.dumps(data)
 
 @app.route('/login', methods=['POST', 'GET'])
-def mentor_login():
+def login():
     if request.method=='POST':
         role=request.json['role']
         name=request.json['name']
         password=request.json['password']
-        q = sqlalchemy.text(f"SELECT id,name,role,department_id FROM users WHERE name='{name}' and password='{password}' and role={role};")
+        q = sqlalchemy.text(f"SELECT id,name,role,department_id FROM user_details_check WHERE name='{name}' and password='{password}' and role_id={role};")
         r = conn.execute(q).fetchall()
         if r:
             data=[dict(i._mapping) for i in r]
@@ -35,29 +35,39 @@ def mentor_login():
             return json.dumps({'Error':'Incorrect details entered'})
 
 @app.route('/register', methods=['POST', 'GET'])
-def coordinator_login():
+def register():
        if request.method=='POST':
         role=request.json['role']
         name=request.json['name']
         password=request.json['password']
         dept_id=request.json['department_id']
         id=request.json['id']
-        q = sqlalchemy.text(f"INSERT INTO users VALUES({id},'{name}',{role},'{password}',{dept_id});")
+        q = sqlalchemy.text(f"INSERT INTO t_users VALUES({id},'{name}','{password}',{dept_id});")
+        conn.execute(q)
+        q = sqlalchemy.text(f"INSERT INTO l_role_user VALUES({id},{role});")
         conn.execute(q)
         conn.commit()
         return json.dumps({'data':'Success'})
 
-@app.route('/dm_login', methods=['POST', 'GET'])
-def dm_login():
-    data="domain mentor logged in"
-    return data
-
-@app.route('/admin_login', methods=['POST', 'GET'])
-def admin_login():
-    data="admin logged in"
-    return data
-
-
+@app.route('/faculty', methods=['POST', 'GET'])
+def faculty():
+    if request.method=='POST':
+        q = sqlalchemy.text(f"SELECT * FROM faculty_table WHERE uid={session[id]};")
+        r = conn.execute(q).fetchall()
+        if r:
+            data=[dict(i._mapping) for i in r]
+            print(data)
+            return json.dumps(data[0])
+        
+@app.route('/mentor', methods=['POST', 'GET'])
+def mentor():
+    if request.method=='POST':
+        q = sqlalchemy.text(f"SELECT * FROM mentor_table;")
+        r = conn.execute(q).fetchall()
+        if r:
+            data=[dict(i._mapping) for i in r]
+            print(data)
+            return json.dumps(data[0])
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001,host="0.0.0.0")
--- server/sqls/schema.sql
+++ server/sqls/schema.sql
@@ -1,22 +1,112 @@
-
--- Create users table
-CREATE TABLE users (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    role INT NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    department_id INT
+-- Create the t_roles table 
+CREATE TABLE t_roles(
+  role_id INT PRIMARY KEY,  
+  designation VARCHAR(24) NOT NULL
+);
+-- Create the t_departments table
+CREATE TABLE t_departments(
+  department_id INT PRIMARY KEY,
+  department_name VARCHAR(24) NOT NULL
 );
 
--- Create courses table
-CREATE TABLE courses (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    department_id INT,
-    outcome VARCHAR(255),
-    status INT,
-    handler_id INT,
-    hours_over INT,
-    total_hours INT,
-    url VARCHAR(255)
+-- Create the t_course_details table
+CREATE TABLE t_course_details (
+  course_code VARCHAR(24) PRIMARY KEY,
+  course_name VARCHAR(24) NOT NULL
 );
+
+-- Create the t_status table 
+CREATE TABLE t_status (
+  status_code INT PRIMARY KEY,
+  status VARCHAR(24) NOT NULL
+);
+
+-- Create the t_users table
+CREATE TABLE t_users (
+  uid INT PRIMARY KEY,
+  name VARCHAR(24) NOT NULL,
+  password VARCHAR(24) NOT NULL,
+  department_id INT NOT NULL, 
+  FOREIGN KEY (department_id) REFERENCES t_departments(department_id)
+);
+
+-- Create the t_course_topics table 
+CREATE TABLE t_course_topics (
+  course_code VARCHAR(24) NOT NULL,
+  outcome VARCHAR(4),
+  topic VARCHAR(24) NOT NULL,
+  topic_id INT PRIMARY KEY,
+  FOREIGN KEY (course_code) REFERENCES t_course_details(course_code)
+);
+
+-- Create the l_role_user table
+CREATE TABLE l_role_user (
+  uid INT NOT NULL,  
+  role_id INT NOT NULL,
+  FOREIGN KEY (uid) REFERENCES t_users(uid),
+  FOREIGN KEY (role_id) REFERENCES t_roles(role_id)
+);
+
+-- Create the l_course_departments table
+CREATE TABLE l_course_departments (
+  course_code VARCHAR(24) NOT NULL,
+  department_id INT NOT NULL,
+  FOREIGN KEY (course_code) REFERENCES  t_course_details(course_code),
+  FOREIGN KEY (department_id) REFERENCES t_departments(department_id)
+);
+
+
+-- Create the t_hours table
+CREATE TABLE t_hours (
+  topic_id INT,
+  total_hours INT,
+ FOREIGN KEY (topic_id) REFERENCES t_course_topics(topic_id)
+ );
+
+
+-- Create the t_topic_links table 
+CREATE TABLE t_topic_links (
+  topic_id INT NOT NULL,
+  handler_id INT NOT NULL,
+  url VARCHAR(24),
+  FOREIGN KEY (topic_id) REFERENCES t_course_topics(topic_id),
+  FOREIGN KEY (handler_id) REFERENCES t_users(uid)
+);
+
+-- Create the t_topic_comments table 
+CREATE TABLE t_topic_comments (
+  topic_id INT NOT NULL,
+  handler_id INT NOT NULL,
+  comment VARCHAR(255),
+  FOREIGN KEY (topic_id) REFERENCES t_course_topics(topic_id),
+  FOREIGN KEY (handler_id) REFERENCES t_users(uid)
+);
+
+-- Create the t_hours_completed table
+CREATE TABLE t_complete_status (
+  hours_completed INT DEFAULT 0,  
+  topic_id INT,  
+  handler_id INT,
+  course_code VARCHAR(24) NOT NULL,
+  status_code INT DEFAULT 0,
+  FOREIGN KEY (course_code) REFERENCES t_course_details(course_code),
+  FOREIGN KEY (status_code) REFERENCES t_status(status_code), 
+  FOREIGN KEY (topic_id) REFERENCES t_course_topics(topic_id),
+  FOREIGN KEY (handler_id) REFERENCES t_users(uid)
+);
+
+
+--view to check user details
+
+create view user_details_check as select u.uid,u.name,u.password,u.department_id,r.role_id from t_users u,l_role_user r where u.uid=r.uid;
+
+--view for faculty table
+
+create view faculty_table as select u.uid,c.course_code,d.course_name,t.topic,t.outcome,c.status_code,c.hours_completed  from t_users u,t_complete_status c,t_course_details d,t_course_topics t where u.uid=c.handler_id and c.course_code=d.course_code and t.topic_id=c.topic_id;
+
+--view for course mentor and domain mentor table
+
+create view mentor_table as select u.uid,c.course_code,d.course_name,t.topic,t.outcome,c.status_code,l.url  from t_users u,t_complete_status c,t_course_details d,t_course_topics t,t_topic_links l where u.uid=c.handler_id and c.course_code=d.course_code and t.topic_id=c.topic_id and l.topic_id=c.topic_id;
+
+-- initialize the roles
+INSERT INTO t_roles (role_id, designation) VALUES (1, 'Faculty'), (2, 'Course Mentor'), (3, 'Domain Mentor'),(4,'Admin');
