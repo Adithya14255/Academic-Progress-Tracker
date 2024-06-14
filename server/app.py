b5fc79868c6f824e2126f556c1837f7a85ae851f
unknown <adithya14255@gmail.com>
2024-06-14 23:01:48 +0530

    LoginPageCSS

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask_cors import CORS,cross_origin
 app = Flask(__name__)
 CORS(app)
 app.secret_key="helloworld"
-engine = sqlalchemy.create_engine("postgresql://admin:admin@192.168.0.248/kgaps")
+engine = sqlalchemy.create_engine("postgresql://admin:admin@192.168.0.247/kgaps")
 conn = engine.connect()
 
 
