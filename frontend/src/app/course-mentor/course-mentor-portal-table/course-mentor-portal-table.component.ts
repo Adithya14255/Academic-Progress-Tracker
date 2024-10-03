f28361995fc7b9764dc73752e403e2fcaebacafb
Mitun <mitun.7557@gmail.com>
2024-10-03 11:48:50 +0530

    Ctable buttons

--- frontend/src/app/course-mentor/course-mentor-portal-table/course-mentor-portal-table.component.ts
+++ frontend/src/app/course-mentor/course-mentor-portal-table/course-mentor-portal-table.component.ts
@@ -1,33 +1,40 @@
 import { Component } from '@angular/core';
 import { CommonModule, Location } from '@angular/common';
-import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms'; // Import FormsModule for ngModel
-import { User } from '../../interfaces/user';
+import { FormsModule, FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../api.service';
-import { ActivatedRoute, Router } from '@angular/router';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-course-mentor-portal-table',
   standalone: true,
-  imports: [CommonModule, FormsModule,ReactiveFormsModule], // Add FormsModule to imports
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
   templateUrl: './course-mentor-portal-table.component.html',
   styleUrls: ['./course-mentor-portal-table.component.css'],
 })
 export class CourseMentorPortalTableComponent {
   data: any;
-  response: any;
-  displayCourseData:any = {course_code:'',course_name:''};
+  displayCourseData: any = { course_code: '', course_name: '' };
   userdata: any;
-  boxcolor: string = 'white';
   editedIndex: number | null = null;
   link: string = '';
-  faculty:any =[];
-  msg:string='';
-  name: string='';
+  faculty: any = [];
+  msg: string = '';
+  name: string = '';
+  displayTable: boolean = false; 
+  displayAddTopic: boolean = false; 
+
+  addTopicForm = this.formBuilder.group({
+    topic: '',
+    course_code: '',
+    outcome: '',
+    total_hours: 0,
+    uid: 0
+  });
+
   checkoutForm = this.formBuilder.group({
-    handler_id: 0,
-    topic_id: 0,
-    link: '',
+    handler_id: [0],
+    topic_id: [0],
+    link: [''],
   });
 
   constructor(
@@ -36,59 +43,77 @@ export class CourseMentorPortalTableComponent {
     private router: Router,
     private formBuilder: FormBuilder
   ) {}
+
   ngOnInit(): void {
     const state = this.location.getState();
-    if (typeof state === 'object' && state !== null) {
+    if (state && typeof state === 'object') {
       this.userdata = state;
       this.name = this.userdata.name;
     }
-    if(this.userdata){
-    this.apiService
-      .getCoordinatorCourse({'uid':this.userdata.uid})
-      .subscribe((response) => {
-        if('response' in response){
-          this.msg=response.response;
-          console.log(response.response);
-        }
-        else{
-        this.displayCourseData=response[0];
-        this.getCoordinatorData();
-        this.apiService.getFacultyInCourse({'course_code':this.displayCourseData.course_code}).subscribe((data) => {
-          this.faculty = data; // Assign the received data to jsonData
+    if (this.userdata) {
+      this.apiService.getCoordinatorCourse({ 'uid': this.userdata.uid })
+        .subscribe((response) => {
+          if ('response' in response) {
+            this.msg = response.response;
+            console.log(response.response);
+          } else {
+            this.displayCourseData = response[0];
+            this.getCoordinatorData();
+            this.apiService.getFacultyInCourse({ 'course_code': this.displayCourseData.course_code })
+              .subscribe((data) => {
+                this.faculty = data;
+              });
+          }
         });
-        }
-      });}
+    }
+  }
+
+  showTable() {
+    this.displayTable = true;  
+    this.displayAddTopic = false;  
+  }
+
+  showAddTopic() {
+    this.displayAddTopic = true;  
+    this.displayTable = false;  
   }
 
   navigateWithData(): void {
     this.router.navigateByUrl('/course-mentor', { state: this.userdata });
   }
-  getCoordinatorData(){
-    this.apiService.getCourseMentorData(this.userdata.uid).subscribe(
-      response => {
-        if('response' in response){
-          this.msg=response.response;
-          console.log(response.response);
-          this.data = [];
 
-        }else{
+  getCoordinatorData() {
+    this.apiService.getCourseMentorData(this.userdata.uid).subscribe(response => {
+      if ('response' in response) {
+        this.msg = response.response;
+        console.log(response.response);
+        this.data = [];
+      } else {
         this.data = response;
-  }});
-    }
+      }
+    });
+  }
+
+  addTopic() {
+    this.addTopicForm.patchValue({
+      course_code: this.displayCourseData.course_code
+    });
+    this.apiService.addTopicData(this.addTopicForm.value).subscribe((data) => {
+      console.log(data);
+      this.msg = "Topic added successfully!";
+      this.addTopicForm.reset();  // Reset form after submission
+      this.displayAddTopic = false;  // Optionally hide the form
+    });
+  }
+
   getBoxColor(value: number): string {
     switch (value) {
-      case 0:
-        return 'white';
-      case 1:
-        return 'orange';
-      case 2:
-        return 'red';
-      case 3:
-        return 'green';
-      case 4:
-        return 'darkblue';
-      default:
-        return 'white'; // Default color
+      case 0: return 'white';
+      case 1: return 'orange';
+      case 2: return 'red';
+      case 3: return 'green';
+      case 4: return 'darkblue';
+      default: return 'white';
     }
   }
 
@@ -96,33 +121,17 @@ export class CourseMentorPortalTableComponent {
     this.editedIndex = index;
   }
 
-  linkupdate(link:string,topic_id: number, uid: number): void {
+  linkupdate(link: string, topic_id: number, uid: number): void {
     this.editedIndex = null;
-    if (topic_id == 0) {
-      return;
-    }
+    if (topic_id === 0) return;
     this.checkoutForm.patchValue({
       handler_id: uid,
       topic_id: topic_id,
       link: link,
     });
-    this.apiService.updateLinkDetails(this.checkoutForm.value).subscribe();
-    this.router
-      .navigateByUrl('/course-mentor-table', { state: this.data })
-      .then(() => {
-        window.location.reload();
-      });
-  }
-
-  addTopicForm = this.formBuilder.group({ topic:"",course_code:'',outcome:"",total_hours:0,uid:0 });
-
-  addTopic() {
-    this.addTopicForm.patchValue({
-      course_code:this.displayCourseData.course_code
-    });
-    this.apiService.addTopicData(this.addTopicForm.value).subscribe((data) => {
-      this.response = data; // Assign the received data to jsonData
-      console.log(this.response);
+    this.apiService.updateLinkDetails(this.checkoutForm.value).subscribe(() => {
+      this.router.navigateByUrl('/course-mentor-table', { state: this.data })
+        .then(() => window.location.reload());
     });
   }
 }
