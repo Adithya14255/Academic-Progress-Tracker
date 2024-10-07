878e99a2584e59ba8dcf2e69136004a793c4c930
Mitun <mitun.7557@gmail.com>
2024-10-07 22:54:24 +0530

    For security

--- server/app.py
+++ server/app.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 app.secret_key = "helloworld"
 engine = sqlalchemy.create_engine(
-    "postgresql://admin:admin@192.168.56.1/kgaps")
+    "postgresql://admin:admin@0.0.0.0/kgaps")
 conn = engine.connect()
 
 
