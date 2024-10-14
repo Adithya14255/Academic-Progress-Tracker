26160c314d5b8918237e8feed0ac755129d56d54
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2024-10-14 11:59:30 +0530

    update the ui in login page :)

--- frontend/src/app/login/course-mentor-login/course-mentor-login.component.ts
+++ frontend/src/app/login/course-mentor-login/course-mentor-login.component.ts
@@ -4,6 +4,7 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { FormGroup,Validators,FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../api.service';
 import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 
 @Component({
@@ -11,7 +12,24 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule,ReactiveFormsModule,RouterLink,RouterLinkActive],
   templateUrl: './course-mentor-login.component.html',
-  styleUrl: './course-mentor-login.component.css'
+  styleUrl: './course-mentor-login.component.css',
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ backgroundColor: 'transparent', opacity: 0 }),
+        animate(1000, style({ backgroundColor: 'white', opacity: 1 }))
+      ])
+    ]),
+    trigger('slideLeft', [
+      transition(':enter', [
+        style({ transform: 'translateX(20vw)' }),
+        animate('500ms 400ms linear', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('500ms  linear', style({ transform: 'translateX(20vw)' }))
+      ])
+    ])
+  ]
 })
 export class CourseMentorLoginComponent {
   data: User = {uid:0,name:'',role_id:0,department_id:0};
--- frontend/src/app/login/domain-mentor-login/domain-mentor-login.component.ts
+++ frontend/src/app/login/domain-mentor-login/domain-mentor-login.component.ts
@@ -3,13 +3,31 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { ApiService } from '../../api.service';
 import { CommonModule } from '@angular/common';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-domain-mentor-login',
   standalone: true,
   imports: [ReactiveFormsModule,RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './domain-mentor-login.component.html',
-  styleUrls: ['./domain-mentor-login.component.css'] // Note the plural 'styleUrls'
+  styleUrls: ['./domain-mentor-login.component.css'],
+  animations: [
+    trigger('fade', [
+      transition('void => *', [
+        style({ backgroundColor: 'transparent', opacity: 0 }),
+        animate(1000, style({ backgroundColor: 'white', opacity: 1 }))
+      ])
+    ]),
+    trigger('slideLeft', [
+      transition(':enter', [
+        style({ transform: 'translateX(20vw)' }),
+        animate('500ms 400ms linear', style({ transform: 'translateX(0)' }))
+      ]),
+      transition(':leave', [
+        animate('500ms  linear', style({ transform: 'translateX(20vw)' }))
+      ])
+    ])
+  ]// Note the plural 'styleUrls'
 })
 export class DomainMentorLoginComponent {
   data:any;
@@ -20,6 +38,8 @@ export class DomainMentorLoginComponent {
     role: 3,
     password: ''
   });
+  ngOnInit(){
+  }
   fetchDomainMentorData() {
     if (this.checkoutForm.valid) {
       this.apiService.postLoginDomainMentorData(this.checkoutForm.value).subscribe(data => {
